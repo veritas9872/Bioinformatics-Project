f456862994cd93d6639c766339bf0f024bf05588
veritas <veritas9872@gmail.com>
2019-12-13 17:58:55 +0900

    Finished Train/Val/Test split function.

--- PPI/utils.py
+++ PPI/utils.py
@@ -33,26 +33,59 @@ def process_graph(adj):
     return sparse_to_info(adj_norm)
 
 
-def mask_graph_edges(adj, val_ratio=0.02, test_ratio=0.02):
+def split_graph_edges(sparse_matrix, val_ratio=0.02, test_ratio=0.02):
     """
     Function for building train/validation/test split in graph.
-    Also removes diagonal elements.
+    Also removes diagonal elements. No seeding available.
     """
-    adj -= sp.diags(adj.diagonal())
-    adj.eliminate_zeros()
+    sparse_matrix -= sp.diags(sparse_matrix.diagonal())
+    sparse_matrix.eliminate_zeros()
 
-    adj_triu = sp.triu(adj)
-    adj_info_triu = sparse_to_info(adj_triu)
-    edge_coords = adj_info_triu[0]
-    edge_coords_all = sparse_to_info(adj)[0]
+    upper_triangular = sp.triu(sparse_matrix)
+    upper_triangular_info = sparse_to_info(upper_triangular)
+    edge_coords = upper_triangular_info[0]
+    edge_coords_all = sparse_to_info(sparse_matrix)[0]
     num_edges = edge_coords_all.shape[0]
     num_val_edges = int(num_edges * val_ratio)
     num_test_edges = int(num_edges * test_ratio)
     num_train_edges = num_edges - num_val_edges - num_test_edges
 
     edge_idx = np.random.permutation(num_edges)
-    train_edges = edge_idx[:num_train_edges]
-    val_edges = edge_idx[num_train_edges:-num_test_edges]
-    test_edges = edge_idx[-num_test_edges:]
+    train_edges = edge_coords[edge_idx[:num_train_edges]]
+    val_edges = edge_coords[edge_idx[num_train_edges:-num_test_edges]]
+    test_edges = edge_coords[edge_idx[-num_test_edges:]]
 
-    # TODO: Finish this function!
+    num_nodes = sparse_matrix.shape[0]
+
+    fake_test_edges = list()
+    while len(fake_test_edges) < num_test_edges:
+        # Sorting forces the indices to be upper triangular. No replacement forces the indices to be non-diagonal.
+        indices = np.random.choice(num_nodes, size=2, replace=False).sort()
+        # The "not in" operator can be used because edge_coords is an (N,2) array and indices is a (2,) vector.
+        # This allows their shapes to be compatible.
+        if (indices not in edge_coords) and (indices not in fake_test_edges):  # Filter out existing edges.
+            fake_test_edges.append(indices)
+
+    fake_val_edges= list()
+    while len(fake_val_edges) < num_val_edges:
+        indices = np.random.choice(num_nodes, size=2, replace=False).sort()
+        # Different from SNAP code but similar in effect.
+        if (indices not in edge_coords) and (indices not in fake_val_edges):
+            fake_val_edges.append(indices)
+
+    # Rebuild the adjacency matrix.
+    data = np.ones(shape=num_train_edges)  # Connections are marked by 1.
+    # Build a sparse matrix where the locations given by the indices in train_edges
+    # are given values of 1 to mark connections.
+    # Coordinates in train_edges must be given as separate vectors, hence the horizontal split.
+    train_matrix = sp.csr_matrix((data, np.hsplit(train_edges)), shape=sparse_matrix.shape)
+    train_matrix += train_matrix.transpose()  # Symmetric matrix should be the same on the transpose.
+
+    return train_matrix, train_edges, val_edges, fake_val_edges, test_edges, fake_test_edges
+
+
+
+
+
+
+
