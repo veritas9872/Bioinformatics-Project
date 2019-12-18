a10996403fb746cbb292b493305216e46d423397
veritas <veritas9872@gmail.com>
2019-12-18 17:44:35 +0900

    Finished implementing PPI GCN. Implemented weight decay as well to improve regularization. Also moved implementation to Jupyter for better recording.

--- PPI/train_ppi.py
+++ PPI/train_ppi.py
@@ -7,33 +7,32 @@ import tensorflow as tf
 from absl import flags
 import scipy.sparse as sp
 import numpy as np
-from tqdm.autonotebook import tqdm
 
-from PPI.utils import load_edgelist_data, sparse_to_info, process_graph, split_graph_edges
+from PPI.utils import load_edgelist_data, to_normalized_sparse_tensor, \
+    split_graph_edges, get_roc_score, to_sparse_tensor
 from model.gcn import GCN
-
-# %%
 
 # Set hyper-parameters.
 FLAGS = flags.FLAGS
 flags.DEFINE_float(name='lr', default=0.01, help='learning rate', lower_bound=0.)
 flags.DEFINE_float(name='val_r', default=0.02, help='Validation set ratio', lower_bound=0., upper_bound=1.)
 flags.DEFINE_float(name='test_r', default=0.02, help='Test set ratio', lower_bound=0., upper_bound=1.)
-flags.DEFINE_integer(name='seed', default=None, help='Seed for data split. Not the seed for model training.')
+flags.DEFINE_integer(name='seed', default=9872, help='Seed for data split. Not the seed for model training.')
 flags.DEFINE_float(name='dropout', default=0.5, help='Dropout rate.', lower_bound=0, upper_bound=1)
 flags.DEFINE_bool(name='bias', default=True, help='Whether to use bias in GCN model.')
-flags.DEFINE_integer(name='h1', default=16, help='Number of hidden features for the first hidden layer of the GCN,'
-                                                 ' which has only 2 hidden layers.', lower_bound=1)
-flags.DEFINE_integer(name='h2', default=16, help='Number of hidden features for the second hidden layer of the GCN,'
-                                                 ' which has only 2 hidden layers.', lower_bound=1)
-flags.DEFINE_integer(name='epochs', default=100, help='Number of training epochs.', lower_bound=1)
+flags.DEFINE_float(name='l2', default=0.005, help='L2 weight decay factor for weights and biases of the model.')
+flags.DEFINE_integer(name='h1', default=4, lower_bound=1,
+                     help='Number of hidden features for the first hidden layer of the GCN.')
+flags.DEFINE_integer(name='h2', default=4, lower_bound=1,
+                     help='Number of hidden features for the second hidden layer of the GCN.')
+flags.DEFINE_integer(name='epochs', default=12, help='Number of training epochs.', lower_bound=1)
 flags.DEFINE_string(name='logs', default='./logs',
                     help='root directory for logs, checkpoints, and other records.')
 FLAGS.mark_as_parsed()  # This is necessary for using FLAGS in jupyter.
 
 # %%
 
-# TODO: Please note that sparse dropout is not implemented properly for graph mode.
+# NOTE: Sparse dropout is not implemented properly for graph mode.
 
 data_path = '../data/yeast.edgelist'
 adj = load_edgelist_data(data_path)  # Get adjacency matrix in CSR format.
@@ -42,7 +41,6 @@ adj = load_edgelist_data(data_path)  # Get adjacency matrix in CSR format.
 num_nodes = adj.shape[0]
 num_edges = adj.sum()
 
-
 # %%
 
 adj_orig = adj - sp.diags(adj.diagonal())  # Removing diagonal elements.
@@ -50,11 +48,10 @@ adj_orig.eliminate_zeros()
 
 # %%
 
-data = split_graph_edges(adj, val_ratio=FLAGS.val_r, test_ratio=FLAGS.test_r, seed=FLAGS.seed)
-adj_train, edges_train, edges_val, edges_val_fake, edges_test, edges_test_fake = data
+adj_train, edges_train, edges_val, edges_val_fake, edges_test, edges_test_fake = \
+    split_graph_edges(adj, val_ratio=FLAGS.val_r, test_ratio=FLAGS.test_r, seed=FLAGS.seed)
 
-norm_edges, norm_data, norm_shape = process_graph(adj_train)
-adj_train_norm = tf.SparseTensor(indices=norm_edges, values=norm_data, dense_shape=norm_shape)
+adj_train_norm = to_normalized_sparse_tensor(adj_train)
 
 
 # %%
@@ -69,33 +66,49 @@ writer = tf.summary.create_file_writer(logdir=str(log_dir))
 
 # %%
 
-lab_edges, lab_data, lab_shape = sparse_to_info(adj_train + sp.identity(num_nodes, dtype=np.float32))
-adj_labels = tf.sparse.to_dense(tf.SparseTensor(indices=lab_edges, values=lab_data, dense_shape=lab_shape))
+adj_train_labels = adj_train + sp.identity(num_nodes, dtype=np.float32)
+adj_train_labels = tf.convert_to_tensor(adj_train_labels.todense())
 
-oh_edges, oh_data, oh_shape = sparse_to_info(sp.identity(num_nodes, dtype=np.float32))
-one_hot = tf.SparseTensor(indices=oh_edges, values=oh_data, dense_shape=oh_shape)
+one_hot = to_sparse_tensor(sp.identity(num_nodes, dtype=np.float32))
 
 position_weight = (num_nodes ** 2 - num_edges) / num_edges
 norm = num_nodes ** 2 / (2 * (num_nodes ** 2 - num_edges))
 
 # %%
 
+print('Beginning Training!')
 with writer.as_default():
     # Training loop
-    for epoch in tqdm(range(1, FLAGS.epochs + 1)):  # Only 1 training iteration per epoch.
+    for epoch in range(1, FLAGS.epochs + 1):  # Only 1 training iteration per epoch.
         tic = time()
         inputs = [one_hot, adj_train_norm]
 
         with tf.GradientTape() as tape:  # Begin gradient calculations from here.
             outputs = model(inputs, training=True)
-            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=adj_labels, logits=outputs))
+            weight_decay = sum(tf.reduce_mean(tf.nn.l2_loss(w)) for w in model.weights)
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=adj_train_labels, logits=outputs))
+            loss += FLAGS.l2 * weight_decay  # Implementing weight decay for further stabilization of training.
         variables = model.trainable_variables
         gradients = tape.gradient(target=loss, sources=variables)
         optimizer.apply_gradients(grads_and_vars=zip(gradients, variables))
 
+        adjacency_recon = outputs.numpy()  # Slow and inefficient but necessary.
+        train_roc, train_ap = get_roc_score(adjacency_recon, real_edges=edges_train, fake_edges=edges_test_fake)
+        val_roc, val_ap = get_roc_score(adjacency_recon, real_edges=edges_val, fake_edges=edges_val_fake)
+
         toc = int(time() - tic)
         # print(f'Epoch {epoch:03d} Time: {toc}s, loss: {float(loss):.3f}')
-        tf.summary.scalar(name='Training Loss', data=loss, step=epoch, description='Training loss for each epoch.')
-    else:
-        writer.flush()
+        tf.summary.scalar(name='Train/Loss', data=loss, step=epoch, description='Training loss for each epoch.')
+        tf.summary.scalar(name='Train/ROC', data=train_roc, step=epoch, description='ROC for training set.')
+        tf.summary.scalar(name='Train/AP', data=train_ap, step=epoch, description='Average Precision for training set.')
+        tf.summary.scalar(name='Val/ROC', data=val_roc, step=epoch, description='ROC for validation set.')
+        tf.summary.scalar(name='Val/AP', data=val_ap, step=epoch, description='Average Precision for validation set.')
+        print(f'Epoch {epoch} >>> Train Loss: {loss:.2f}, Train ROC: {train_roc:.4f}, Train AP: {train_ap:.4f},'
+              f' Val ROC: {val_roc:.4f}, VAL AP: {val_ap:.4f}')
+
+    # After training loop is finished.
+    writer.flush()
+    print('Training is Finished!')
+    test_roc, test_ap = get_roc_score(adjacency_recon, real_edges=edges_test, fake_edges=edges_test_fake)
+    print(f'Test ROC: {test_roc:.4f}, Test AP: {test_ap:.4f}')
 
--- PPI/utils.py
+++ PPI/utils.py
@@ -2,17 +2,18 @@ import numpy as np
 import scipy.sparse as sp
 from sklearn.metrics import roc_auc_score, roc_curve, average_precision_score
 import networkx as nx
+import tensorflow as tf
 from absl import flags
 
 FLAGS = flags.FLAGS
 
 
-def load_edgelist_data(file_path):
+def load_edgelist_data(file_path: str):
     graph = nx.read_edgelist(file_path)
     return nx.adjacency_matrix(graph)
 
 
-def sparse_to_info(sparse_matrix):
+def sparse_to_info(sparse_matrix: sp.spmatrix):
     if not sp.isspmatrix_coo(sparse_matrix):
         sparse_matrix = sp.coo_matrix(sparse_matrix, shape=sparse_matrix.shape)  # Transform into COO format.
     # Edge coordinates are stored as a long column of pairs. Shape of (N, 2).
@@ -22,19 +23,25 @@ def sparse_to_info(sparse_matrix):
     return edges, data, shape
 
 
-def process_graph(adj):
-    adj = sp.coo_matrix(adj, shape=adj.shape, dtype=np.float32)
-    adj_ = adj + sp.identity(adj.shape[0], dtype=np.float32)  # Add the identity matrix for inclusion of self features.
-    row_sum = np.array(adj_.sum(axis=1))
+def to_sparse_tensor(sparse_matrix):
+    edges, data, shape = sparse_to_info(sparse_matrix)
+    return tf.SparseTensor(indices=edges, values=data, dense_shape=shape)
+
+
+def to_normalized_sparse_tensor(sparse_matrix: sp.spmatrix):
+    sparse_matrix = sp.coo_matrix(sparse_matrix, shape=sparse_matrix.shape, dtype=np.float32)
+    # Add the identity matrix for inclusion of self features.
+    sparse_matrix += sp.identity(sparse_matrix.shape[0], dtype=np.float32)  # Data type conversion is important.
+    row_sum = np.array(sparse_matrix.sum(axis=1))
     # deg is the D\hat^(-1/2) matrix in the paper, the diagonal matrix of the identity augmented adjacency matrix.
     deg = sp.diags((row_sum ** -0.5).ravel())
     # Normalized adjacency matrix.
-    adj_norm = sp.coo_matrix(adj_.dot(deg).transpose().dot(deg), shape=adj.shape, dtype=np.float32)
+    adj_norm = sp.coo_matrix(sparse_matrix.dot(deg).transpose().dot(deg), shape=sparse_matrix.shape, dtype=np.float32)
     adj_norm.eliminate_zeros()
-    return sparse_to_info(adj_norm)
+    return to_sparse_tensor(adj_norm)
 
 
-def split_graph_edges(sparse_matrix, val_ratio=0.02, test_ratio=0.02, seed=None):
+def split_graph_edges(sparse_matrix: sp.spmatrix, val_ratio=0.02, test_ratio=0.02, seed=None):
     """
     Function for building train/validation/test split in graph.
     Also removes diagonal elements. No seeding available.
@@ -43,7 +50,7 @@ def split_graph_edges(sparse_matrix, val_ratio=0.02, test_ratio=0.02, seed=None)
     np.random.seed(seed)  # For reproducibility of data split, etc.
 
     # Removing diagonal elements.
-    sparse_matrix -= sp.diags(sparse_matrix.diagonal(), shape=sparse_matrix.shape, dtype=np.float32)
+    sparse_matrix -= sp.diags(sparse_matrix.diagonal(), shape=sparse_matrix.shape)
     sparse_matrix.eliminate_zeros()
 
     upper_triangular = sp.triu(sparse_matrix)
@@ -57,58 +64,59 @@ def split_graph_edges(sparse_matrix, val_ratio=0.02, test_ratio=0.02, seed=None)
     num_train_edges = num_edges - num_val_edges - num_test_edges
 
     edge_idx = np.random.permutation(num_edges)
-    train_edges = edge_coords[edge_idx[:num_train_edges]].astype(dtype=np.float32)
-    val_edges = edge_coords[edge_idx[num_train_edges:-num_test_edges]].astype(dtype=np.float32)
-    test_edges = edge_coords[edge_idx[-num_test_edges:]].astype(dtype=np.float32)
+    train_edges = edge_coords[edge_idx[:num_train_edges]]
+    val_edges = edge_coords[edge_idx[num_train_edges:-num_test_edges]]
+    test_edges = edge_coords[edge_idx[-num_test_edges:]]
 
     num_nodes = upper_triangular.shape[0]
     edge_coordinates = set(tuple(coord) for coord in edge_coords)
 
-    def make_fake_edges(num_fake_edges):
-        fake_edges = edge_coordinates.copy()
-        while len(fake_edges) < num_edges + num_fake_edges:
-            # Sorting forces the indices to be upper triangular. No replacement forces the indices to be non-diagonal.
-            indices = tuple(np.sort(np.random.choice(num_nodes, size=2, replace=False), axis=-1))
-            fake_edges.add(indices)  # Efficiently adds only unique indices.
-        return np.array(list(fake_edges - edge_coordinates))  # Remove original coordinates.
+    # Generate fake edges for testing later.
+    fake_test_edges = edge_coordinates.copy()  # Use sets for fast removal of duplicates.
+    while len(fake_test_edges) < (num_edges + num_test_edges):
+        # Indices are always upper triangular when sorted. Not allowing replacement removes diagonal elements.
+        indices = tuple(np.sort(np.random.choice(num_nodes, size=2, replace=False), axis=-1))
+        fake_test_edges.add(indices)  # Numpy arrays cannot be hashed, hence the need to convert to tuples.
 
-    fake_test_edges = make_fake_edges(num_test_edges)
-    fake_val_edges = make_fake_edges(num_val_edges)
+    # Generate fake edges for validation for later.
+    fake_val_edges = fake_test_edges.copy()  # Deep copy necessary to prevent adding to original set.
+    while len(fake_val_edges) < (num_edges + num_test_edges + num_val_edges):
+        indices = tuple(np.sort(np.random.choice(num_nodes, size=2, replace=False), axis=-1))
+        fake_val_edges.add(indices)
+
+    # Turn the set into an array of shape (n,2), removing the unnecessary elements from each set.
+    fake_val_edges = np.array(list(fake_val_edges - fake_test_edges))
+    fake_test_edges = np.array(list(fake_test_edges - edge_coordinates))
 
     # Rebuild the adjacency matrix.
-    data = np.ones(shape=num_train_edges, dtype=np.float32)  # Connections are marked by 1.
+    data = np.ones(shape=num_train_edges)  # Connections are marked by 1.
     # Build a sparse matrix where the locations given by the indices in train_edges
     # are given values of 1 to mark connections.
     # Coordinates in train_edges must be given as separate vectors, hence the horizontal split.
     train_matrix = sp.csr_matrix((data, (train_edges[:, 0], train_edges[:, 1])),
-                                 shape=sparse_matrix.shape, dtype=np.float32)
+                                 shape=sparse_matrix.shape)
     # Symmetric matrix should be the same on the transpose.
     train_matrix += train_matrix.transpose()
 
     # Returning the edge coordinates to symmetric form.
-    train_edges = np.concatenate([train_edges, np.fliplr(train_edges)], axis=0).astype(dtype=np.float32)
-    val_edges = np.concatenate([val_edges, np.fliplr(val_edges)], axis=0).astype(dtype=np.float32)
-    test_edges = np.concatenate([test_edges, np.fliplr(test_edges)], axis=0).astype(dtype=np.float32)
+    train_edges = np.concatenate([train_edges, np.fliplr(train_edges)], axis=0)
+    val_edges = np.concatenate([val_edges, np.fliplr(val_edges)], axis=0)
+    test_edges = np.concatenate([test_edges, np.fliplr(test_edges)], axis=0)
 
     return train_matrix, train_edges, val_edges, fake_val_edges, test_edges, fake_test_edges
 
 
-def get_roc_score(adjacency_original, real_edges, fake_edges):
+def get_roc_score(adjacency_recon: np.ndarray, real_edges, fake_edges):
 
-    # adjacency_recon = tf.matmul(embedding, embedding, transpose_b=True)
-    real_idx = ((edge[0], edge[1]) for edge in real_edges)
-    fake_idx = ((edge[0], edge[1]) for edge in fake_edges)
+    real_preds = np.stack([adjacency_recon[edge[0], edge[1]] for edge in real_edges], axis=0)
+    real_preds = (1 / (1 + np.exp(-real_preds)))  # Sigmoid. (1 / (1 + e^-x)).
 
-    # real_recons = np.stack([adjacency_recon[idx] for idx in real_idx], axis=0)
-    real_preds = np.exp(np.stack([adjacency_original[idx] for idx in real_idx], axis=0))
-    real_preds /= (real_preds + 1)  # Sigmoid (e^x/(e^x + 1)).
-
-    # fake_recons = np.stack([adjacency_recon[idx] for idx in fake_idx], axis=0)
-    fake_preds = np.exp(np.stack([adjacency_original[idx] for idx in fake_idx], axis=0))
-    fake_preds /= (fake_preds + 1)  # Sigmoid (e^x/(e^x + 1)).
+    fake_preds = np.stack([adjacency_recon[edge[0], edge[1]] for edge in fake_edges], axis=0)
+    fake_preds = (1 / (1 + np.exp(-fake_preds)))  # Sigmoid. (1 / (1 + e^-x)).
 
     all_preds = np.concatenate([real_preds, fake_preds], axis=0)
     all_labels = np.concatenate([np.ones(len(real_preds)), np.zeros(len(fake_preds))], axis=0)
+
     roc_score = roc_auc_score(y_true=all_labels, y_score=all_preds)
     ap_score = average_precision_score(y_true=all_labels, y_score=all_preds)
 
