32204e1614d46df8b620e2b292de60d000b2ccbe
junyul94 <junyul94@kaist.ac.kr>
2019-12-20 16:23:04 +0900

    atom_features paddes with 0

--- MPP/preprocess_bace.py
+++ MPP/preprocess_bace.py
@@ -23,15 +23,18 @@ if __name__ == '__main__':
     print('Beginning featurization process. (CONV)')
     molecule_features = featurizer.featurize(molecules)
     atom_features = [feature.get_atom_features() for feature in molecule_features]
-    max_num = max(map(len,([mol for mol in atom_features]))) # calculate maximum number of atom number
+    atom_nums = list(map(len, ([mol for mol in atom_features])))
+    max_num = max(atom_nums) # calculate maximum number of atom number
 
     adjacency_lists = [feature.get_adjacency_list() for feature in molecule_features]
     adjacency_matrices = [adj_list_to_adj_matrix(adj_list, max_num) for adj_list in adjacency_lists]
+    pad_nums = [max_num - n for n in atom_nums]
+    atom_features_padded = [np.pad(adj,((0,pn),(0,0)),'constant',constant_values=(0)) for (adj,pn) in zip(atom_features, pad_nums)]
 
     activity_labels = np.array(active, dtype=bool)
-    bace_data_conv = {'atom_features': atom_features,
+    bace_data_conv = {'atom_features': atom_features_padded,
                     'adjacency_matrices': adjacency_matrices, 'activity_labels': activity_labels}
-
+    
     print('Beginning featurization process. (ECFP)')
     ecfp_degree = 2 # can be changed
     ecfp_power = 9 # can be changed
--- MPP/preprocess_hiv.py
+++ MPP/preprocess_hiv.py
@@ -17,20 +17,23 @@ if __name__ == '__main__':
     active = hiv_df['HIV_active']
     
     print('Beginning transformation to molecules.')
-    featurizer_conv = dc.feat.ConvMolFeaturizer()   
+    featurizer = dc.feat.ConvMolFeaturizer()   
     molecules = [Chem.MolFromSmiles(mol) for mol in smiles]
 
     print('Beginning featurization process. (CONV)')
-    molecule_features = featurizer_conv.featurize(molecules)
+    molecule_features = featurizer.featurize(molecules)
     atom_features = [feature.get_atom_features() for feature in molecule_features]
-    max_num = max(map(len,([mol for mol in atom_features]))) # calculate maximum number of atom number
+    atom_nums = list(map(len, ([mol for mol in atom_features])))
+    max_num = max(atom_nums) # calculate maximum number of atom number
 
     adjacency_lists = [feature.get_adjacency_list() for feature in molecule_features]
     adjacency_matrices = [adj_list_to_adj_matrix(adj_list, max_num) for adj_list in adjacency_lists]
+    pad_nums = [max_num - n for n in atom_nums]
+    atom_features_padded = [np.pad(adj,((0,pn),(0,0)),'constant',constant_values=(0)) for (adj,pn) in zip(atom_features, pad_nums)]
 
     activity_labels = np.array(active, dtype=bool)
     hiv_data_conv = {'atom_features': atom_features,
-                    'adjacency_matrices': adjacency_matrices, 'activity_labels': activity_labels}
+                    'adjacency_matrices': adjacency_matrices_padded, 'activity_labels': activity_labels}
     
     print('Beginning featurization process. (ECFP)')
     ecfp_degree = 2 # can be changed
