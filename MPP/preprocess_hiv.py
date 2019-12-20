1da10dfbabdb94052d38b583aa33a8ca4630177c
veritas <veritas9872@gmail.com>
2019-12-20 17:26:06 +0900

    Fixed errors and made some readability improvements.

--- MPP/preprocess_hiv.py
+++ MPP/preprocess_hiv.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 import pandas as pd
 
+
 def adj_list_to_adj_matrix(adj_list: list, max_num: int):
     matrix = np.zeros(shape=(max_num, max_num), dtype=bool)  # Minimizing data storage requirements.
     for row in range(len(adj_list)):
@@ -11,37 +12,41 @@ def adj_list_to_adj_matrix(adj_list: list, max_num: int):
             matrix[row, col] = True
     return matrix
 
+
 if __name__ == '__main__':
     hiv_df = pd.read_csv('../data/HIV.csv')
     smiles = hiv_df['smiles']
     active = hiv_df['HIV_active']
-    
+
     print('Beginning transformation to molecules.')
-    featurizer = dc.feat.ConvMolFeaturizer()   
+    featurizer = dc.feat.ConvMolFeaturizer()
     molecules = [Chem.MolFromSmiles(mol) for mol in smiles]
 
     print('Beginning featurization process. (CONV)')
     molecule_features = featurizer.featurize(molecules)
     atom_features = [feature.get_atom_features() for feature in molecule_features]
-    atom_nums = list(map(len, ([mol for mol in atom_features])))
-    max_num = max(atom_nums) # calculate maximum number of atom number
+    atom_nums = [len(mol) for mol in atom_features]
+    max_num_atoms = max(atom_nums)  # calculate maximum number of atom number
 
     adjacency_lists = [feature.get_adjacency_list() for feature in molecule_features]
-    adjacency_matrices = [adj_list_to_adj_matrix(adj_list, max_num) for adj_list in adjacency_lists]
-    pad_nums = [max_num - n for n in atom_nums]
-    atom_features_padded = [np.pad(adj,((0,pn),(0,0)),'constant',constant_values=(0)) for (adj,pn) in zip(atom_features, pad_nums)]
+    adjacency_matrices = [adj_list_to_adj_matrix(adj_list, max_num_atoms) for adj_list in adjacency_lists]
+    pad_nums = [max_num_atoms - n for n in atom_nums]
+    atom_features_padded = list()
+    for (adj, pn) in zip(atom_features, pad_nums):
+        atom_features_padded.append(np.pad(adj, pad_width=((0, pn), (0, 0)), mode='constant', constant_values=0))
 
     activity_labels = np.array(active, dtype=bool)
     hiv_data_conv = {'atom_features': atom_features_padded,
-                    'adjacency_matrices': adjacency_matrices_padded, 'activity_labels': activity_labels}
-    
+                     'adjacency_matrices': adjacency_matrices, 'activity_labels': activity_labels}
+
     print('Beginning featurization process. (ECFP)')
-    ecfp_degree = 2 # can be changed
-    ecfp_power = 9 # can be changed
-    ecfp_features = [dc.feat.rdkit_grid_featurizer.compute_ecfp_features(mol, ecfp_degree, ecfp_power) for mol in molecules]
+    ecfp_degree = 2  # can be changed
+    ecfp_power = 9  # can be changed
+    ecfp_features = [dc.feat.rdkit_grid_featurizer.compute_ecfp_features(
+        mol, ecfp_degree=ecfp_degree, ecfp_power=ecfp_power) for mol in molecules]
 
     activity_labels = np.array(active, dtype=bool)
-    hiv_data_ecfp = {'ecfp_features': ecfp_features, 'activity_labels': activity_labels}    
+    hiv_data_ecfp = {'ecfp_features': ecfp_features, 'activity_labels': activity_labels}
 
     print('Beginning saving!')
     # Saving data to pickle file for easier data loading later.
@@ -50,4 +55,4 @@ if __name__ == '__main__':
         pickle.dump(obj=hiv_data_conv, file=file, protocol=pickle.HIGHEST_PROTOCOL)
 
     with open('./hiv_data_ecfp.pickle', mode='xb') as file:
-        pickle.dump(obj=hiv_data_ecfp, file=file, protocol=pickle.HIGHEST_PROTOCOL)        
+        pickle.dump(obj=hiv_data_ecfp, file=file, protocol=pickle.HIGHEST_PROTOCOL)
